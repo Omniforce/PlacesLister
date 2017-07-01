6ddf19d0907e220c7ca4dad04d41b85cd31505b8
Brendan Ross <brendanros@gmail.com>
2017-06-30 22:41:11 -0400

    project v2

---
+++ PlacesLister v2/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PlacesLister v2
+//
+//  Created by Brendan Ross on 6/30/17.
+//  Copyright © 2017 Brendan Ross. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        return true
+    }
+}
+
---
+++ PlacesLister v2/Helpers/Place.swift
@@ -0,0 +1,14 @@
+//
+//  Place.swift
+//  PlacesLister v2
+//
+//  Created by Brendan Ross on 6/30/17.
+//  Copyright © 2017 Brendan Ross. All rights reserved.
+//
+
+struct Place {
+    let id: String
+    let name: String
+    let location: String
+    let photo: String?
+}
